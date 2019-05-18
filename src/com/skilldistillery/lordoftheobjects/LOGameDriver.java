1145032dd1e2885974e67a3aa4834b339d6c3ded
Dunweiz8157 <dunweiz8157@gmail.com>
2019-05-18 09:54:49 -0600

    2nd Initial

---
+++ src/com/skilldistillery/lordoftheobjects/LOGameDriver.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.lordoftheobjects;
+
+public class LOGameDriver {
+	public static void main(String[] args) {
+		LOGameDriver game = new LOGameDriver();
+		game.run();
+	}
+
+	public void run() {
+		
+	}
+}
